aed30e8aa27f50be15f34f6b00addace4b5932ae
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-07-04 19:28:45 +0300

    feature: implement getBalance and sendBet in wrapper, add autotests

--- tests/Lottery.spec.ts
+++ tests/Lottery.spec.ts
@@ -36,5 +36,46 @@ describe('Lottery', () => {
         // the check is done inside beforeEach
         // blockchain and lottery are ready to use
     });
+
+    it('should not bet when sending wrong amount', async () => {
         const wrongBetResult = await lottery.sendBet_(deployer.getSender(), Lottery.fixedStake + toNano('0.01'));
+        expect(wrongBetResult.transactions).toHaveTransaction({
+            inMessageBounced: true,
+        });
+    });
+
+    it('should bet when sending correct amount', async () => {
+        const correctBetResult = await lottery.sendBet_(deployer.getSender(), Lottery.fixedStake);
+        expect(correctBetResult.transactions).not.toHaveTransaction({
+            inMessageBounced: true,
+        });
+    });
+
+    it('should either increase contract balance by approx. 1 and decrease sender balance by ~ or vise verse', async () => {
+        // probably smaller; not sure if it's safe to test the specific amount in sandbox
+        const expectedGasMargin = toNano('0.01');
+        const expectedWinAmount = toNano('1');
+
+        const senderBalanceBefore = await deployer.getBalance();
+        const contractBalanceBefore = await lottery.getBalance();
+
+        await lottery.sendBet(deployer.getSender());
+
+        const senderBalanceAfter = await deployer.getBalance();
+        const contractBalanceAfter = await lottery.getBalance();
+
+        const senderBalanceChange = senderBalanceAfter - senderBalanceBefore;
+        const senderBalanceChangeAbs = senderBalanceChange > 0n ? senderBalanceChange : -senderBalanceChange;
+        expect(senderBalanceChangeAbs - expectedWinAmount).toBeLessThan(expectedGasMargin);
+        expect(senderBalanceChangeAbs - expectedWinAmount).toBeGreaterThan(-expectedGasMargin);
+
+        const contractBalanceChange = contractBalanceAfter - contractBalanceBefore;
+        const contractBalanceChangeAbs = contractBalanceChange > 0n ? contractBalanceChange : -contractBalanceChange;
+        expect(contractBalanceChangeAbs - expectedWinAmount).toBeLessThan(expectedGasMargin);
+        expect(contractBalanceChangeAbs - expectedWinAmount).toBeGreaterThan(-expectedGasMargin);
+
+        const balancesSumChange = senderBalanceChange + contractBalanceChange;
+        expect(balancesSumChange).toBeLessThan(0n);
+        expect(balancesSumChange).toBeGreaterThan(-expectedGasMargin - expectedGasMargin);
+    });
 });
--- wrappers/Lottery.ts
+++ wrappers/Lottery.ts
@@ -35,4 +35,25 @@ export class Lottery implements Contract {
             body: beginCell().endCell(),
         });
     }
+
+    // public methods
+    async getBalance(provider: ContractProvider) {
+        const state = await provider.getState();
+        return state.balance;
+    }
+
+    // sendBet_ is only used in tests
+    async sendBet_(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(0, 32) // op == 0 for bet
+                // ? when we need to send query id?
+                .endCell(),
+        })
+    }
+    async sendBet(provider: ContractProvider, via: Sender) {
+        await this.sendBet_(provider, via, Lottery.fixedStake);
+    }
 }
